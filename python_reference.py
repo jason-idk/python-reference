d30407a675acc15db0b7b0421a997f33b2eaf02c
JLH993 <jasonhensley93@gmail.com>
2020-03-28 22:51:18 -0500

    Adding more info...

--- python_reference.py
+++ python_reference.py
@@ -19,8 +19,10 @@ my_dictionary_fmt = {'apple':2.25,
                      'milk':4.80}
 my_flex_dictionary = {'key':'value',           # Key value pair
                       'keylist':[1,2,3],       # Key contains list as value. Can pull from list like: my_flex_dictionary['keylist'][2]
-                      'kik':{'insidekey':100}} # Key inside of key (call using my_flex_dictionary['kik']['insidekey']
-                                               # ^ returns 100
+                      'kik':{'insidekey':100}, # Key inside of key (call using my_flex_dictionary['kik']['insidekey']
+                      'ree':['one', 'two', 'three']}                                                 # ^ returns 100
+my_tuple = (1,2,3)
+
 
 # Strings, indexing, and slicing
 #-------------------------------#
@@ -98,3 +100,29 @@ print(my_dictionary)
 # Print key1 value...
 print(my_dictionary['key1'])
 print(my_dictionary_fmt['milk'])
+
+# Print specific from list and format with method...
+print(my_flex_dictionary['ree'][2].upper())
+
+# Add new key value pair to dictionary...
+my_flex_dictionary['newkey'] = 300
+print(my_flex_dictionary)
+
+# List all keys in dictionary...
+print(my_dictionary_fmt.keys())
+
+# List all values...
+print(my_dictionary_fmt.values())
+
+
+# Tuples (Immutable)
+#-------------------------------#
+# Tuples are like lists, but immutable and have less methods...
+t2 = ('one', 2, 'one')
+print(t2)
+
+# Count instances of 'one' in t2...
+print(t2.count('one'))
+
+# Show index of where 'one' appears in tuple the first time...
+print(t2.index('one'))
